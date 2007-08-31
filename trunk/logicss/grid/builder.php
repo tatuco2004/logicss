fd52d5b398400df7aa82fe20fd9486b5376f39d9
logicoder <logicoder@e25b3f08-5c38-0410-a779-f3b9e6a44fdf>
2007-08-31 16:55:30 +0000

    Started grid builder.

---
+++ trunk/logicss/grid/builder.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * LogiCSS - Logic CSS framework
+ * http://logicss.googlecode.com
+ * http://logicoder.com/logicss/
+ *
+ * Copyright (c) 2007 Marco Del Tongo, http://logicoder.com/logicss/AUTHORS
+ * Licensed under the MIT license, see http://logicoder.com/logicss/LICENSE
+ *
+ * Revision: $Rev$
+ */
+?>
+/**
+ * LogiCSS - Logic CSS framework
+ * http://logicss.googlecode.com
+ * http://logicoder.com/logicss/
+ *
+ * NAME......:    
+ * WIDTH.....:    
+ * STYLE.....:    [ ] Elastic    [ ] Fluid      [ ] Fixed
+ * H-ALIGN...:    [ ] Left       [ ] Center     [ ] Right
+ * V-ALIGN...:    [ ] Top        [ ] Center     [ ] Bottom
+ * SPACING...:    [ ] Margin     [ ] Padding    [ ] None
+ * PRE~APP...:    [ ] Margin     [ ] Padding    [ ] None
+ *
+ * Copyright (c) 2007 Marco Del Tongo, http://logicoder.com/logicss/AUTHORS
+ * Licensed under the MIT license, see http://logicoder.com/logicss/LICENSE
+ */
